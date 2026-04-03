27286981856a231acba4f832842fb818d2f4572b
root <root@ser124828463549.local>
2026-04-03 02:24:07 +0000

    test: fix X html title backtracking regression case

--- test_x_html_title_backtracking.py
+++ test_x_html_title_backtracking.py
@@ -3,15 +3,18 @@ import time
 import core
 
 
-X_FREEZE_HTML = """
-<html><head>
-<meta charset=\"utf-8\">
-<meta content=\""" + ("x" * 220000) + "\" property=\"og:title\">\n<title>fallback title</title>\n</head><body></body></html>\n"
+X_FREEZE_HTML = (
+    '<html><head>'
+    '<meta charset="utf-8">'
+    + '<meta content="' + ('x' * 220000) + '" property="og:title">'
+    + '<title>fallback title</title>'
+    + '</head><body></body></html>'
+)
 
 
 def test_extract_title_from_html_avoids_catastrophic_backtracking():
     started = time.time()
     title = core.extract_title_from_html(X_FREEZE_HTML)
     elapsed = time.time() - started
-    assert elapsed < 1.0, f"extract_title_from_html too slow: {elapsed:.3f}s"
-    assert title == "fallback title" or title == ("x" * 220000)
+    assert elapsed < 1.0, f'extract_title_from_html too slow: {elapsed:.3f}s'
+    assert title == ('x' * 220000)
